c796d30e788bddad623b2af941ebd5facd899428
RoguePenguin06 <moss.s.harry@gmail.com>
2024-12-09 18:51:14 +0000

    chore: adding more assets

--- PlateRace/game.py
+++ PlateRace/game.py
@@ -2,14 +2,19 @@ import pygame
 import time
 import math
 
+TRACK = pygame.image.load("assests/Track.png")
+CAR = pygame.image.load("assets/SportsCar.png")
+GRASS = pygame.image.load("assets/grass.png")
 
 
 class PlateRace:
     def __init__(self):
         pygame.init()
+
+        WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
         pygame.display.set_caption("PlateRace")
         
-        self.screen = pygame.display.set_mode((800, 600))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         
     def main_loop(self):
         while True:
