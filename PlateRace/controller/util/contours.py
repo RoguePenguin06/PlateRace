f41ffef899744445b7987accc7e95a453a3d5c7c
MgnMtn <mematon1@sheffield.ac.uk>
2024-12-13 06:28:53 +0000

    feat: finding contours

--- PlateRace/controller/util/contours.py
+++ PlateRace/controller/util/contours.py
@@ -14,6 +14,8 @@ import numpy as np
 img = cv.imread('./controller.png')
 cv.imshow("Cats", img)
 
+blank = np.zeros(img.shape, dtype='uint8')
+cv.imshow("Blank", blank)
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 cv.imshow("Gray", gray)
@@ -21,13 +23,19 @@ cv.imshow("Gray", gray)
 # This can help with more complex shapes
 # blur = cv.GaussianBlur(gray, (5,5), cv.BORDER_DEFAULT)
 
-# canny = cv.Canny(img, 125, 175)
-# cv.imshow("Canny Edges", canny)
+canny = cv.Canny(img, 125, 175)
+cv.imshow("Canny Edges", canny)
 
+# This reduced the contours found. Thresholding binarizes the image ie black or white
 ret, thresh = cv.threshold(gray, 125, 255, cv.THRESH_BINARY) 
+cv.imshow("Thresh", thresh)
 
-
-contours, hierarchies = cv.findContours(canny, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+contours, hierarchies = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
 print(f'{len(contours)} contour(s) found!')
+
+
+# draw the contours onto the blank canvas (-1 because we want all the contours), last number is thickness
+cv.drawContours(blank, contours, -1, (0,0,255), 2)
+cv.imshow("Contours Drawn", blank)
 
 cv.waitKey(0)
