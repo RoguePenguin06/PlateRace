5d85db9a8e96e2608666317fc0114c5c160ad5de
RoguePenguin06 <moss.s.harry@gmail.com>
2024-12-16 11:58:57 +0000

    feat: car go vroom

--- PlateRace/game.py
+++ PlateRace/game.py
@@ -8,7 +8,7 @@ CAR = resize_image(pygame.image.load("PlateRace\\assets\SportsCar.png"), 0.5)
 GRASS = resize_image(pygame.image.load("PlateRace\\assets\grass.png"), 3)
 FINISH = resize_image(pygame.image.load("PlateRace\\assets\Finish.png"), 2.6)
 
-angle = 0
+angle_D = 0
 
 
 class AbstractCar:
@@ -16,24 +16,58 @@ class AbstractCar:
         self.img = self.IMG
         self.max_velocity = max_velocity
         self.velocity = 0
-        self.angle = 90
+        self.angle = 0
         self.x, self.y = self.START_POS
+        self.acceleration = 0.1
         
     def rotate(self):
-        global angle
+        global angle_D
         
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
-            angle += 5
+            angle_D += 5
         if keys[pygame.K_d]:
-            angle -= 5
+            angle_D -= 5
+        if angle_D < 0:
+            angle_D += 360
+        if angle_D > 360:
+            angle_D -= 360
 
-
-        self.angle = angle
+        self.angle = angle_D
         
     def draw(self):
         blit_rotate_center(WIN, self.img, (self.x, self.y), self.angle)
+
+    def move(self):
+        self.change_velocity()
+
+        angle_R = angle_D * (math.pi/180)
+
+        if angle_D <= 90:
+            self.H_velocity = -self.velocity * math.sin(angle_R)
+            self.V_velocity = -self.velocity * math.cos(angle_R)
+
+        elif angle_D <= 180:
+            self.H_velocity = -self.velocity * math.cos(angle_R-(math.pi/2))
+            self.V_velocity = self.velocity * math.sin(angle_R-(math.pi/2))
+
+        elif angle_D <= 270:
+            self.H_velocity = self.velocity * math.sin(angle_R-math.pi)
+            self.V_velocity = self.velocity * math.cos(angle_R-math.pi)
+
+        elif angle_D <= 360:
+            self.H_velocity = self.velocity * math.cos(angle_R-((3*math.pi)/2))
+            self.V_velocity = -self.velocity * math.sin(angle_R-((3*math.pi)/2))
+
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            self.x += self.H_velocity
+            self.y += self.V_velocity
+
+    def change_velocity(self):
+        self.velocity = min(self.velocity + self.acceleration, self.max_velocity)
         
 
 class PlayerCar(AbstractCar):
@@ -69,19 +103,17 @@ class PlateRace:
                 
     def _game_logic(self):
         player_car.rotate()
+        player_car.move()
         
 
-        
-    
+       
     def _draw(self):
         self.screen.blit(GRASS, (0, 0))
         self.screen.blit(TRACK, (0, 0))
         self.screen.blit(FINISH, (831, 300))
         player_car.draw()
-        #pygame.display.flip()
         pygame.display.update()
         
-
 
 clock = pygame.time.Clock()
 
