2b5bca40945cd87db5f529efe974b6257aa46eb8
RoguePenguin06 <moss.s.harry@gmail.com>
2025-01-09 12:11:11 +0000

    feat: grass slows you, finish line adds to laps, you can win the game

--- PlateRace/game.py
+++ PlateRace/game.py
@@ -1,59 +1,82 @@
 import pygame
 import time
 import math
-from utilities import resize_image, blit_rotate_center
+from utilities import resize_image, blit_rotate_center, getDeltaTime
 
 TRACK = resize_image(pygame.image.load("PlateRace\\assets\Track.png"), 2.5)
-CAR = resize_image(pygame.image.load("PlateRace\\assets\SportsCar.png"), 0.5)
+TRACK_MASK = pygame.mask.from_surface(TRACK)
+
+CAR_BLUE = resize_image(pygame.image.load("PlateRace\\assets\SportsCar(Blue).png"), 0.5)
+CAR_RED = resize_image(pygame.image.load("PlateRace\\assets\SportsCar(Red).png"), 0.5)
+
 GRASS = resize_image(pygame.image.load("PlateRace\\assets\grass.png"), 3)
 FINISH = resize_image(pygame.image.load("PlateRace\\assets\Finish.png"), 2.6)
+FINISH_MASK = pygame.mask.from_surface(FINISH)
+FINISH_POS = (831, 300)
+
+P1WIN = resize_image(pygame.image.load("PlateRace\\assets\P1Win.png"), 2.5)
+P2WIN = resize_image(pygame.image.load("PlateRace\\assets\P2Win.png"), 2.5)
 
 angle_D = [0, 0]
 direction = 0
+deltaTime = 0
+gamePlaying = True
+quitTimer = 3
 
 
 class AbstractCar:
     def __init__(self, max_velocity, player_num):
-        self.img = self.IMG
+        self.max_velocity_onTrack = max_velocity
         self.max_velocity = max_velocity
         self.velocity = 0
         self.angle = 0
         self.x, self.y = self.START_POS
         self.acceleration = 0.1
         self.deceleration = 0.5
-        self.PLAYER_NUM = player_num
+        self.lap = 0
+        self.finish_timer = 2
         
-        if self.PLAYER_NUM == 1:
-            self.controls = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
-        elif self.PLAYER_NUM == 2:
-            self.controls = [pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT]
+        if player_num != 0:
+            self.PLAYER_NUM = player_num
         
-    def rotate(self):
-        global angle_D
-        
-        self.angle = angle_D[self.PLAYER_NUM - 1]
+            if self.PLAYER_NUM == 1:
+                self.controls = [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d]
+                self.img = CAR_BLUE
+            elif self.PLAYER_NUM == 2:
+                self.controls = [pygame.K_UP, pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT]
+                self.img = CAR_RED
         
     def draw(self):
         blit_rotate_center(WIN, self.img, (self.x, self.y), self.angle)
-
-    
-
-    def change_velocity(self):
-        global direction
-        keys = pygame.key.get_pressed()
-        if keys[self.controls[0]]:
-            self.velocity = min(self.velocity + self.acceleration, self.max_velocity)
-            direction = 1
-        elif keys[self.controls[2]]:
-            self.velocity = min(self.velocity + self.acceleration, self.max_velocity)
-            direction = -0.25
+        
+    def collide(self, mask, x=0, y=0):
+        car_mask = pygame.mask.from_surface(self.img)
+        offset = (int(self.x - x), int(self.y - y))
+        poi = mask.overlap(car_mask, offset)
+        return poi
+        
+    def onTrack(self):
+        if self.collide(TRACK_MASK):
+            self.max_velocity = self.max_velocity_onTrack
         else:
-            self.velocity = max(self.velocity - self.deceleration, 0)
+            self.max_velocity = self.max_velocity_onTrack * 0.25
+    
+    def finish_line_check(self):
+        global deltaTime
+        self.finish_timer += deltaTime
+        if self.finish_timer >= 2:
+            if self.collide(FINISH_MASK, *FINISH_POS):
+                self.lap += 1
+                self.finish_timer = 0
+     
+    def raceEnd(self):
+        if self.lap > 3:
+            return True
         
 
 class PlayerCar(AbstractCar):
-    IMG = CAR
     START_POS = (420, 500)
+    
     
     def move(self):
         global angle_D
@@ -71,6 +94,7 @@ class PlayerCar(AbstractCar):
         self.rotate()
 
         self.change_velocity()
+        self.onTrack()
 
         angle_R = angle_D[self.PLAYER_NUM - 1] * (math.pi/180)
 
@@ -90,10 +114,27 @@ class PlayerCar(AbstractCar):
             self.H_velocity = self.velocity * math.cos(angle_R-((3*math.pi)/2))
             self.V_velocity = -self.velocity * math.sin(angle_R-((3*math.pi)/2))
 
-
         self.x += self.H_velocity * direction
-        self.y += self.V_velocity * direction          
-    
+        self.y += self.V_velocity * direction
+            
+        
+    def rotate(self):
+        global angle_D
+        
+        self.angle = angle_D[self.PLAYER_NUM - 1]
+        
+    def change_velocity(self):
+        global direction
+        keys = pygame.key.get_pressed()
+        if keys[self.controls[0]]:
+            self.velocity = min(self.velocity + self.acceleration, self.max_velocity)
+            direction = 1
+        elif keys[self.controls[2]]:
+            self.velocity = min(self.velocity + self.acceleration, self.max_velocity)
+            direction = -0.25
+        else:
+            self.velocity = max(self.velocity - self.deceleration, 0)
+            
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()        
 
 def window():
@@ -110,11 +151,18 @@ class PlateRace:
         self.screen = window()
         
     def main_loop(self):
+        global gamePlaying
+        global quitTimer
         while True:
             clock.tick(60)
             self._handle_input()
-            self._game_logic()
-            self._draw()
+            if gamePlaying == True:
+                self._draw()
+                self._game_logic()
+            else:
+                quitTimer -= getDeltaTime()
+                if quitTimer <= 0:
+                    quit()
             
     def _handle_input(self):
         for event in pygame.event.get():
@@ -122,15 +170,31 @@ class PlateRace:
                 quit()
                 
     def _game_logic(self):
+        global deltaTime
+        global gamePlaying
         player_1_car.move()
         player_2_car.move()
+
+        player_1_car.finish_line_check()
+        player_2_car.finish_line_check()
         
+        if player_1_car.raceEnd():
+            gamePlaying = False
+            self.screen.blit(P1WIN, (0, 0))
+            pygame.display.update()
+        if player_2_car.raceEnd():
+            gamePlaying = False
+            self.screen.blit(P2WIN, (0, 0))
+            pygame.display.update()
+            
+           
+        deltaTime = getDeltaTime()
 
        
     def _draw(self):
         self.screen.blit(GRASS, (0, 0))
         self.screen.blit(TRACK, (0, 0))
-        self.screen.blit(FINISH, (831, 300))
+        self.screen.blit(FINISH, FINISH_POS)
         player_1_car.draw()
         player_2_car.draw()
         pygame.display.update()
--- PlateRace/utilities.py
+++ PlateRace/utilities.py
@@ -1,4 +1,5 @@
 import pygame
+getTicksLastFrame = 0
 
 def resize_image(img, factor):
         size = round(img.get_width() * factor), round(img.get_height() * factor)
@@ -8,3 +9,10 @@ def blit_rotate_center(win, image, top_left, angle):
     rotated_image = pygame.transform.rotate(image, angle)
     new_rect = rotated_image.get_rect(center=image.get_rect(topleft=top_left).center)
     win.blit(rotated_image, new_rect.topleft)
+    
+def getDeltaTime():
+    global getTicksLastFrame
+    t = pygame.time.get_ticks()
+    deltaTime = (t - getTicksLastFrame) / 1000.0
+    getTicksLastFrame = t  
+    return deltaTime
